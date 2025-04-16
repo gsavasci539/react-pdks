a0a372eb981715eae3965b1bea1a856b6f449376
gsavasci539 <gsavasci539@gmail.com>
2025-04-16 23:25:53 +0300

    Add files via upload

---
+++ build/precache-manifest.700e91f4c2f1d7eb2507add689bffc81.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6711772ff951a283f293d7d1cb2bca28",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac2d4abfaad94cbcae12",
+    "url": "/static/js/2.0c297521.chunk.js"
+  },
+  {
+    "revision": "1ef8e8448e193d81e4ff",
+    "url": "/static/js/main.02a652b6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
